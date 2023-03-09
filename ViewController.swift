c4f11395facd6c9e2e8584a7916a1c4d760756b7
ryskeldievislam <islamryskeldiev@icloud.com>
2023-03-09 20:30:17 +0600

    initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson2-5
+//
+//  Created by Mac on 9/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("month 5")
+    }
+
+
+}
+
